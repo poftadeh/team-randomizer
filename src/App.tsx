62d4d39a473f8dde16d0393e5c76699da7789e62
Pouya Oftadeh <poftadeh2@gmail.com>
2019-08-17 20:41:17 -0400

    fix: eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import PlayerCount from './screens/PlayerCount';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="d-flex flex-column align-items-center mt-5">
       <PlayerCount></PlayerCount>
