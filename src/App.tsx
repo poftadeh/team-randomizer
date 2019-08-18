572119eee7b2cd1924766eb1b5d2e6685b6b33f5
Pouya Oftadeh <poftadeh2@gmail.com>
2019-08-17 20:43:44 -0400

    refactor: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import PlayerCount from './screens/PlayerCount';
 
-function App(): JSX.Element {
-  return (
-    <div className="d-flex flex-column align-items-center mt-5">
-      <PlayerCount></PlayerCount>
-    </div>
-  );
+class App extends Component {
+  render(): JSX.Element {
+    return (
+      <div className="d-flex flex-column align-items-center mt-5">
+        <PlayerCount></PlayerCount>
+      </div>
+    );
+  }
 }
 
 export default App;
