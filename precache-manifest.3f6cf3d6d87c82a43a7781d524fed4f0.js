82f8f15316e762f105a3f714e19c827fe59d9537
Pouya Oftadeh <poftadeh2@gmail.com>
2019-08-19 04:15:40 -0400

    Updates

---
+++ precache-manifest.3f6cf3d6d87c82a43a7781d524fed4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bf3720fbdccc332b8b463333f69d77",
+    "url": "/team-randomizer/index.html"
+  },
+  {
+    "revision": "c1bdaba5d111341489dc",
+    "url": "/team-randomizer/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9a4985cf26efb7a54b6d",
+    "url": "/team-randomizer/static/css/main.c73be388.chunk.css"
+  },
+  {
+    "revision": "c1bdaba5d111341489dc",
+    "url": "/team-randomizer/static/js/2.8b84004c.chunk.js"
+  },
+  {
+    "revision": "9a4985cf26efb7a54b6d",
+    "url": "/team-randomizer/static/js/main.eabfd2f4.chunk.js"
+  },
+  {
+    "revision": "3d5db93c47fb881f26fe",
+    "url": "/team-randomizer/static/js/runtime~main.f879ea84.js"
+  }
+]);
